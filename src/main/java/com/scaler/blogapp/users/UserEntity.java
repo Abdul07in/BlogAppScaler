ef6a141c4c189cece1d9190d3ae73bea17016a34
Abdul Majeed Kanoor <abdulkanoor@gmail.com>
2024-02-15 22:47:57 +0530

    Entities and Repositories Created
    Signed-off-by: Abdul Majeed Kanoor <abdulkanoor@gmail.com>

---
+++ src/main/java/com/scaler/blogapp/users/UserEntity.java
@@ -0,0 +1,38 @@
+package com.scaler.blogapp.users;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+@Entity(name = "users")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    @NonNull
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    @NonNull
+    private String email;
+
+    @Column(name = "bio", nullable = true)
+    @Nullable
+    private String bio;
+
+    @Column(name = "image", nullable = true)
+    @Nullable
+    private String image;
+
+
+}
